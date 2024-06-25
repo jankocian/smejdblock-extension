549e0d47dc82d44321cf86d3108534aedac26f00
jankocian <mail@jankocian.com>
2024-06-25 17:03:35 +0200

    Blocked tab tweaking

--- pages/blocked-tab/src/BlockedTab.tsx
+++ pages/blocked-tab/src/BlockedTab.tsx
@@ -1,48 +1,55 @@
-import '@src/BlockedTab.css';
-import '@src/BlockedTab.scss';
-import { useStorageSuspense, withErrorBoundary, withSuspense } from '@chrome-extension-boilerplate/shared';
-import { exampleThemeStorage } from '@chrome-extension-boilerplate/storage';
-import { ComponentPropsWithoutRef } from 'react';
+import { withErrorBoundary, withSuspense } from '@chrome-extension-boilerplate/shared';
 
 const BlockedTab = () => {
-  const theme = useStorageSuspense(exampleThemeStorage);
-
   return (
-    <div className="App" style={{ backgroundColor: theme === 'light' ? '#eee' : '#222' }}>
-      <header className="App-header" style={{ color: theme === 'light' ? '#222' : '#eee' }}>
-        <img src={chrome.runtime.getURL('blocked-tab/logo.svg')} className="App-logo" alt="logo" />
-        <p>
-          <h1>Blocked page!</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-          style={{ color: theme === 'light' ? '#0281dc' : undefined, marginBottom: '10px' }}>
-          Learn React
-        </a>
-        <h6>The color of this paragraph is defined using SASS.</h6>
-        <ToggleButton>Toggle theme</ToggleButton>
-      </header>
-    </div>
+    <main className="bg-red-100 min-h-screen py-20 px-6">
+      <div className="w-full p-8 md:p-16 max-w-[600px] mx-auto text-slate-700 bg-white rounded-3xl shadow-lg text-lg">
+        <img src={chrome.runtime.getURL('blocked-tab/logo.png')} className="w-28 mb-6" alt="ŠmejdBlock Logo" />
+
+        <div className="flex flex-col gap-y-3">
+          <h1 className="text-xl md:text-2xl font-semibold mb-3 text-red-600">Riziková stránka zablokována</h1>
+          <p>
+            Rozšíření ŠmejdBlock zablokovalo tuto stránku, protože se nachází na{' '}
+            <a
+              href="https://www.coi.cz/pro-spotrebitele/rizikove-e-shopy/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline text-red-600 hover:text-red-700">
+              seznamu rizikových e-shopů
+            </a>{' '}
+            České obchodní inspekce či{' '}
+            <a
+              href="https://www.sukl.cz/leciva/webove-stranky-s-nelegalnimi-nabidkami-leciv"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline text-red-600 hover:text-red-700">
+              stránek s&nbsp;nelegálními nabídkami léčiv
+            </a>{' '}
+            Státního ústavu pro kontrolu léčiv.
+          </p>
+          <p>
+            Tyto webové stránky se vyznačují převedším nedodržováním zákonných povinností a obtížnou vymahatelností
+            spotřebitelských práv. Nabízené přípravky bývají neschválené či bez ověřené kvality a účinnosti.
+          </p>
+        </div>
+
+        <div className="mt-8 flex flex-wrap gap-4">
+          <button
+            onClick={() => window.history.go(-2)}
+            className="border border-red-600 text-red-600 hover:text-red-700 rounded-md px-6 py-3 hover:bg-red-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+            Zpět na předchozí stránku
+          </button>
+          <a
+            href="https://smejdblock.cz"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="bg-red-700 text-white px-6 py-3 rounded-md hover:bg-red-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
+            Více informací
+          </a>
+        </div>
+      </div>
+    </main>
   );
 };
 
-const ToggleButton = (props: ComponentPropsWithoutRef<'button'>) => {
-  const theme = useStorageSuspense(exampleThemeStorage);
-  return (
-    <button
-      className={
-        props.className +
-        ' ' +
-        'font-bold mt-4 py-1 px-4 rounded shadow hover:scale-105 ' +
-        (theme === 'light' ? 'bg-white text-black' : 'bg-black text-white')
-      }
-      onClick={exampleThemeStorage.toggle}>
-      {props.children}
-    </button>
-  );
-};
-
-export default withErrorBoundary(withSuspense(BlockedTab, <div> Loading ... </div>), <div> Error Occur </div>);
+export default withErrorBoundary(withSuspense(BlockedTab, <div> Načítám... </div>), <div> Error </div>);
