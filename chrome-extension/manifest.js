23228dea0a88b742d50c2b68d69c94165cdd654e
jankocian <mail@jankocian.com>
2024-06-25 17:03:08 +0200

    fix manifest to use v3

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -26,9 +26,7 @@ const manifest = Object.assign(
     name: '__MSG_extensionName__',
     version: packageJson.version,
     description: '__MSG_extensionDescription__',
-    permissions: ['storage', 'webRequest', 'webRequestBlocking', 'tabs', 'http://*/', 'https://*/'].concat(
-      sidePanelConfig.permissions,
-    ),
+    permissions: ['storage', 'activeTab', 'tabs', 'alarms'].concat(sidePanelConfig.permissions),
     options_page: 'options/index.html',
     background: {
       service_worker: 'background.iife.js',
