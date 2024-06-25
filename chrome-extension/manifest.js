d6849f8af9f6bf7d036d0a7d07d3d69dfc7dc434
jankocian <mail@jankocian.com>
2024-06-25 15:33:04 +0200

    Manifest

--- chrome-extension/manifest.js
+++ chrome-extension/manifest.js
@@ -26,7 +26,9 @@ const manifest = Object.assign(
     name: '__MSG_extensionName__',
     version: packageJson.version,
     description: '__MSG_extensionDescription__',
-    permissions: ['storage'].concat(sidePanelConfig.permissions),
+    permissions: ['storage', 'webRequest', 'webRequestBlocking', 'tabs', 'http://*/', 'https://*/'].concat(
+      sidePanelConfig.permissions,
+    ),
     options_page: 'options/index.html',
     background: {
       service_worker: 'background.iife.js',
@@ -37,26 +39,26 @@ const manifest = Object.assign(
       default_icon: 'icon-34.png',
     },
     chrome_url_overrides: {
-      newtab: 'new-tab/index.html',
+      // newtab: 'new-tab/index.html',
     },
     icons: {
       128: 'icon-128.png',
     },
-    content_scripts: [
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        js: ['content/index.iife.js'],
-      },
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        js: ['content-ui/index.iife.js'],
-      },
-      {
-        matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-        css: ['content.css'], // public folder
-      },
-    ],
-    devtools_page: 'devtools/index.html',
+    // content_scripts: [
+    //   {
+    //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //     js: ['content/index.iife.js'],
+    //   },
+    //   {
+    //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //     js: ['content-ui/index.iife.js'],
+    //   },
+    //   {
+    //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+    //     css: ['content.css'], // public folder
+    //   },
+    // ],
+    // devtools_page: 'devtools/index.html',
     web_accessible_resources: [
       {
         resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
